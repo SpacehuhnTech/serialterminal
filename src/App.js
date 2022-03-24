31a4f45dbafdea0e75ee860bd7155daa0e400e1f
Spacehuhn <github@spacehuhn.de>
2022-03-24 23:31:43 +0100

    Adding the important stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+
 import Header from './components/Header'
+
+import version from './version.js'
 
 function App() {
   return (
-    <Header/>
+    <Box>
+      <Header/>
+
+      { /* Made in Germany :D */}
+      <Box sx={{ mx: 'auto', mt: 5 }}>
+        <Typography
+          align='center'
+          display='block'>
+          Made with ❤️ by <Link href='https://spacehuhn.com' target='_blank' underline='hover' color='inherit'>Spacehuhn</Link>
+        </Typography>
+      </Box>
+
+      { /* Version */}
+      <Typography
+        variant='caption'
+        align='center'
+        display='block'
+        sx={{ color: '#ddd' }}>
+        {version.name}
+      </Typography>
+    </Box>
   );
 }
 
