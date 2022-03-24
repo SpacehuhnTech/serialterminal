1f9112352f07a4ef5adb51fc330bd240a683feab
Spacehuhn <github@spacehuhn.de>
2022-03-24 23:40:07 +0100

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import Box from '@mui/material/Box'
-import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
 
 import Header from './components/Header'
-
-import version from './version.js'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Box>
       <Header/>
-
-      { /* Made in Germany :D */}
-      <Box sx={{ mx: 'auto', mt: 5 }}>
-        <Typography
-          align='center'
-          display='block'>
-          Made with ❤️ by <Link href='https://spacehuhn.com' target='_blank' underline='hover' color='inherit'>Spacehuhn</Link>
-        </Typography>
-      </Box>
-
-      { /* Version */}
-      <Typography
-        variant='caption'
-        align='center'
-        display='block'
-        sx={{ color: '#ddd' }}>
-        {version.name}
-      </Typography>
+      <Footer/>
     </Box>
   );
 }
