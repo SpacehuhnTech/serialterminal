4323ea10eea8d5471ad607778ee42afe9c9af9f7
Spacehuhn <github@spacehuhn.de>
2022-04-18 13:16:12 +0200

    External stylesheet for terminal css

--- src/components/TerminalOutput.js
+++ src/components/TerminalOutput.js
@@ -13,96 +13,7 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import ListItemIcon from '@mui/material/ListItemIcon'
-import TerminalIcon from '@mui/icons-material/Terminal';
-
-const preCSS = {
-    position: 'relative',
-    height: 'calc(100vh - 200px)',
-    minHeight: '10em',
-    padding: '0',
-    margin: '0',
-}
-
-const buttonGroupCSS = {
-    position: 'absolute',
-    right: 0,//'1em',
-    top: 0,//'1em',
-    margin: 0,
-    padding: 0,
-    background: 'rgba(32, 33, 36, .6)',
-    color: '#fff',
-}
-
-const buttonCSS = {
-    color: '#fff',
-    '&.Mui-disabled': {
-        color: '#aaa'
-    },
-    '&.MuiButtonGroup-groupedTextHorizontal:not(:last-child)': {
-        borderColor: '#777'
-    }
-}
-
-const boxCSS = {
-    height: '100%',
-    width: '100%',
-    background: '#202124',
-    borderRadius: '4px',
-    padding: 0,
-    margin: 0,
-    display: 'flex',
-    overflowX: 'scroll',
-    overflowY: 'scroll',
-    flexDirection: 'column-reverse',
-    resize: 'vertical',
-    '&::-webkit-scrollbar': {
-        width: '8px',
-        height: '8px',
-    },
-    '&::-webkit-scrollbar-track': {
-        background: 'transparent',
-        borderRadius: '4px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-        background: '#555',
-        borderRadius: '4px',
-    },
-    '&::-webkit-scrollbar-corner': {
-        background: 'transparent',
-        borderRadius: '4px',
-    },
-    '&::-webkit-resizer': {
-        display: 'none',
-    },
-}
-
-const codeCSS = {
-    margin: '.5rem',
-    padding: 0,
-    fontFamily: [
-        'Roboto Mono',
-        'monospace',
-    ].join(','),
-    fontWeight: 300,
-}
-
-const lineCSS = {
-    'output': {
-        color: '#fff'
-    },
-    'userInput': {
-        color: '#fbbc05',
-        fontWeight: 700,
-        display: 'block',
-        borderBottom: '1px solid',
-    }
-}
-
-const timeCSS = {
-    fontWeight: 300,
-    color: '#aaa',
-    width: '100%'
-}
+import TerminalIcon from '@mui/icons-material/Terminal'
 
 const TerminalOutput = (props) => {
     // User input history window
@@ -114,36 +25,35 @@ const TerminalOutput = (props) => {
     }
 
     return (
-        <pre style={preCSS}>
+        <pre className='terminalOutput'>
 
             { /* Buttons */}
-            <ButtonGroup variant='text' sx={buttonGroupCSS}>
+            <ButtonGroup variant='text' className='terminalButtons'>
 
                 { /* Clear */}
-                <Button sx={buttonCSS} onClick={handleClear}>
+                <Button onClick={handleClear}>
                     <HighlightOffIcon color='inherit' />
                 </Button>
 
                 { /* History */}
-                <Button sx={buttonCSS} onClick={() => setHistoryOpen(true)}>
+                <Button onClick={() => setHistoryOpen(true)}>
                     <HistoryIcon color='inherit' />
                 </Button>
 
                 { /* Settings */}
-                <Button sx={buttonCSS} onClick={props.openSettings}>
+                <Button onClick={props.openSettings}>
                     <SettingsIcon color='inherit' />
                 </Button>
             </ButtonGroup>
 
             { /* Text */}
-            <Box sx={boxCSS}>
-                <code style={codeCSS}>
+            <Box className='codeContainer'>
+                <code>
                     {props.history.filter(line => (line.type === 'output' || props.echo)).map((line, i) => (
-                        <span key={i} style={lineCSS[line.type]}>
-                            <span style={timeCSS}>{props.time && `${line.time.toTimeString().substring(0, 8)} `}</span>
-                            {line.value}
-                            <br />
-                        </span>
+                        <p key={i}>
+                            <span className='time'>{props.time && `${line.time.toTimeString().substring(0, 8)} `}</span>
+                            <span className={line.type}>{line.value}</span>
+                        </p>
                     ))}
                 </code>
             </Box>
